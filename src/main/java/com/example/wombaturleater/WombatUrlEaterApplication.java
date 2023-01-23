f7ddfccf8e3b0b163c3fd26e0babed829a77a9b5
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-01-23 11:35:27 -0500

    Initial commit

---
+++ src/main/java/com/example/wombaturleater/WombatUrlEaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.wombaturleater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WombatUrlEaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WombatUrlEaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wombaturleater/WombatUrlEaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wombaturleater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WombatUrlEaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
